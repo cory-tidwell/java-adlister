eda2cab6425d1b620290bf955725fb3b974e1a26
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-06-14 14:46:45 -0400

    jsp-jstl-exercises

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -9,7 +9,14 @@ public class CounterServlet extends HttpServlet {
     private int counter = 0;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String reset = request.getParameter("reset");
+        if(reset != null) {
+            if (reset.equals("0")) {
+                counter = -1;
+            }
+        }
         counter += 1;
         response.getWriter().println("<h1>The count is " + counter + ".</h1>");
     }
+
 }
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cory
+  Date: 6/14/22
+  Time: 10:32 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello there, ${name}!</h1>
+</body>
+</html>
