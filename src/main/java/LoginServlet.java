3f6ea3e3d459fc83384821be999f35da50ed2e89
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-06-16 16:05:18 -0400

    session-cookies

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,18 +8,39 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        // redirect if the user is not an admin
+        // cast to a boolean data type to properly compare
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean validAttempt2 = username.equals("cory") && password.equals("testtest");
 
-        if (validAttempt) {
+
+
+        if (username.equals("admin") && password.equals("password")) {
+            request.getSession().setAttribute("user", username);
+            response.sendRedirect("/profile");
+        } else if (username.equals("tito") && password.equals("jackson")) {
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+//        if (validAttempt) {
+//            response.sendRedirect("/profile");
+//        } else if (validAttempt2) {
+//            response.sendRedirect("/profile");
+//        } else {
+//            response.sendRedirect("/login");
+//        }
     }
 }
